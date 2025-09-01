a24181081ae6105b51ff67adba3b19ef949fcf56
Laurinhalindinhapontocom <csa16-6234@colegiosantoantonio.com.br>
2025-09-01 14:21:15 -0300

    Update student.py

--- student.py
+++ student.py
@@ -1,7 +1,11 @@
 # Leia uma linha com o número do cartão
 numero = input()
-
-# TODO: implemente a verificação pelo algoritmo de Luhn
+ímpares = []
+for i in numeros[-1::-2]:
+  ímpares.append(int(i))
+pares = []
+for i in numeros[-2::-2]:
+  if 2*int(i)# TODO: implemente a verificação pelo algoritmo de Luhn
 # Siga as dicas do README.
 
 # Ao final, imprima exatamente:
